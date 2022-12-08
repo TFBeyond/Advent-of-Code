d5251bd4c2f41d9a6c3426d3d787c2fd66fbb9bb
TFBeyond <rob_kemp2001@yahoo.co.uk>
2022-12-08 19:57:17 +0000

    Proper multiple variable assignment. Offloaded error handling to function.

--- 2022/go/Day_1_code.go
+++ 2022/go/Day_1_code.go
@@ -8,17 +8,18 @@ import (
 	"strings"
 )
 
-func main() {
-	file, err := os.Open("C:/Users/rob_k/Documents/advent_of_code/2022/inputs/Day_1_input.txt")
+func handle_err(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func main() {
+	file, err := os.Open("C:/Users/rob_k/Documents/advent_of_code/2022/inputs/Day_1_input.txt")
+	handle_err(err)
+
 	scanner := bufio.NewScanner(file)
-	tally := 0
-	third_place := 0
-	second_place := 0
-	highest_score := 0
-	// var highest_score, second_place, third_place = 0, 0, 0
+	tally, third_place, second_place, highest_score := 0, 0, 0, 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		if strings.TrimSpace(line) == "" {
@@ -29,14 +30,13 @@ func main() {
 			tally = 0
 		} else {
 			num, err := strconv.Atoi(line)
+			handle_err(err)
 			tally += num
-			if err != nil {
-				panic(err)
-			}
 		}
-
 	}
+
 	fmt.Println("pt1 answer is:", highest_score)
 	total := third_place + second_place + highest_score
 	fmt.Println("pt2 answer is:", third_place, second_place, highest_score, "for a total of:", total)
+
 }
