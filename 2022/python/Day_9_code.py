73a047635e59ac09eb91d770c47fbd7884746dda
TFBeyond <rob_kemp2001@yahoo.co.uk>
2022-12-09 13:01:27 +0000

    pt2 implemented. Code refactored to support arbitary number of knots.

--- 2022/python/Day_9_code.py
+++ 2022/python/Day_9_code.py
@@ -1,52 +1,75 @@
-H_x=H_y=T_x=T_y = 0
-visited_spots = set()
+knot_list = [] #A list of lists stores the x and y of each knot.
+num_of_knots = 10 #Easy to make it support an arbitrary number of knots, so why no-- Why shouldn't I?
 
-def tail_catchup_check(H_x,H_y,T_x,T_y):
-    rope_distance = 0
-    if H_x > T_x+1 or H_x < T_x-1:
-        rope_distance += abs(H_x - T_x)
+#Sets handle filtering out duplicates for us.
+pt1_visited_spots = set()
+pt2_visited_spots = set()
 
-    if H_y > T_y+1 or H_y < T_y-1:
-        rope_distance += abs(H_y - T_y)
+#If a knot needs to go left or right, figure out which then push it one step in that direction
+def close_x_distance(last_coords, knot_coords): 
+            if last_coords[0] > knot_coords[0]:
+                knot_coords[0] += 1
+            elif last_coords[0] < knot_coords[0]:
+                knot_coords[0] -= 1
 
-    if rope_distance > 1:
-        if H_x > T_x:
-            T_x += 1
-        elif H_x < T_x:
-            T_x -= 1
+# Same, but for up/down
+def close_y_distance(last_coords, knot_coords):
+            if last_coords[1] > knot_coords[1]:
+                knot_coords[1] += 1
+            elif last_coords[1] < knot_coords[1]:
+                knot_coords[1] -= 1
 
-        if H_y > T_y:
-            T_y += 1
-        elif H_y < T_y:
-            T_y -= 1
+close_in_the_distance = "Whispers~ falling silently, drift on the wind..."
 
-    visited_spots.add((T_x,T_y))
-    return(H_x,H_y,T_x,T_y)
+
+def lets_implement_snake(knot_list, pt1_visited_spots, pt2_visited_spots): #...or ~= snake, at least.
+    for knot_num, knot_coords in enumerate(knot_list[1:], start=1): # We never need to touch the head knot.
+
+        last_coords = knot_list[knot_num-1] #This is why we start from 1. idx 0 - 1 makes idx -1, which is the _last_ element in the list.
+        rope_x_distance=rope_y_distance = 0 #Track these individually to know whether to go in one axis or both axes.
+
+        # distance is the difference between coordinates of the knot and the one further up the chain
+        # abs shaves the + or - off to turn subtraction into difference.
+        rope_x_distance += abs(last_coords[0] - knot_coords[0]) 
+        rope_y_distance += abs(last_coords[1] - knot_coords[1])
+        total_distance = rope_x_distance + rope_y_distance
+
+        if total_distance > 2: #Do diagonal first if both axes need movement.
+            close_x_distance(last_coords, knot_coords)
+            close_y_distance(last_coords, knot_coords)
+        elif rope_x_distance >= 2:
+            close_x_distance(last_coords, knot_coords)
+        elif rope_y_distance >= 2:
+            close_y_distance(last_coords, knot_coords)
+
+        knot_list[knot_num] = knot_coords
+ 
+    pt1_visited_spots.add(tuple(knot_list[1]))
+    pt2_visited_spots.add(tuple(knot_list[9]))
+    return(knot_list)
+        
+for _ in range(num_of_knots): #Generate our arbitrary number of knots.
+    knot_list.append([0,0])
 
 with open("C:/Users/rob_k/Documents/advent_of_code/2022/inputs/Day_9_input.txt", 'r') as input:
     for line in input.readlines():
         line = line.strip().split(' ')
-        instruction = line[0]
-        repeats = int(line[1])
+        instruction = line[0] #First split is the direction
+        repeats = int(line[1]) #Second split is the number of steps
 
-        match instruction:
-            case 'R':
-                for _ in range(repeats):
-                    H_x += 1
-                    H_x,H_y,T_x,T_y = tail_catchup_check(H_x,H_y,T_x,T_y)
-            case 'U':
-                for _ in range(repeats):
-                    H_y += 1
-                    H_x,H_y,T_x,T_y = tail_catchup_check(H_x,H_y,T_x,T_y)
-            case 'L':
-                for _ in range(repeats):
-                    H_x -= 1
-                    H_x,H_y,T_x,T_y = tail_catchup_check(H_x,H_y,T_x,T_y)
-            case 'D':
-                for _ in range(repeats):
-                    H_y -= 1
-                    H_x,H_y,T_x,T_y = tail_catchup_check(H_x,H_y,T_x,T_y)
+        for _ in range(repeats): #Move the head knot x steps...
+            match instruction:
+                case 'R': # ... to the right.
+                    knot_list[0][0] += 1 # +1 to left/right!
+                case 'U': # ... up.
+                    knot_list[0][1] += 1 # +1 to up/down!
+                case 'L': # ... to the left.
+                    knot_list[0][0] -= 1 # -1 to left/right!
+                case 'D': # ... down.
+                    knot_list[0][1] -= 1 # -1 to up/down!
 
+            #With the head knot moved, lets have everyone else catch up!
+            knot_list = lets_implement_snake(knot_list, pt1_visited_spots, pt2_visited_spots)
 
-    
-print(len(visited_spots))
+print("The second knot in line went to {} different places. Golly!".format(len(pt1_visited_spots)))
+print("Knot number {} went to {} different places. Wow!".format(num_of_knots, len(pt2_visited_spots)))
