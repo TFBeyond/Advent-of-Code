aacd96a6fa3eece224ca70b25a57c423902f11e5
TFBeyond <rob_kemp2001@yahoo.co.uk>
2022-12-11 17:34:55 +0000

    Pt2 basic completion

--- 2022/python/Day_11_code.py
+++ 2022/python/Day_11_code.py
@@ -1,10 +1,10 @@
 import aocutils
-import re
-monkey_dict = {}
-#monkey_num = 0
+
 monkey_business = 0
+monkey_dict = {}
+divisor_product = 1
 input = aocutils.getSplitAoCInput()
-round_count = 1
+
 for line in input:
     match line[0]:
         case '':
@@ -18,53 +18,57 @@ for line in input:
             monkey_dict[monkey_num]['operation'] = [line[-2],line[-1]]
         case 'Test:':
             monkey_dict[monkey_num]['test_divisor'] = int(line[-1])
+            divisor_product *= monkey_dict[monkey_num]['test_divisor']
+            monkey_dict[monkey_num]['divisor_product'] = divisor_product
+
         case 'If':
             if line[1] == 'true:':
                 monkey_dict[monkey_num]['if_true_target'] = line[-1]
             elif line[1] == 'false:':
                 monkey_dict[monkey_num]['if_false_target'] = line[-1]
-            else:
-                print("Uh-oh?")
 
-while round_count <= 20:
+def monkeyGame(round_target, full_monkey_panic=True):
+    for round_count in range(round_target):
+        for monkey_v in monkey_dict.values():
+            for item_worry_level in monkey_v['inventory'][:]:
+
+                monkey_v['inventory'].remove(item_worry_level)
+                
+                if monkey_v['operation'][1] == 'old':
+                    second_arg = item_worry_level
+                else:
+                    second_arg = int(monkey_v['operation'][1])
+                match monkey_v['operation'][0]:
+                    case '+':
+                        item_worry_level += second_arg
+                    case '*':
+                        item_worry_level *= second_arg
+                    case '-':
+                        item_worry_level -= second_arg
+                    case '/':
+                        item_worry_level /= second_arg
+
+                if full_monkey_panic == False:
+                    item_worry_level //= 3
+
+                if item_worry_level > divisor_product:
+                    item_worry_level %= divisor_product
+
+                #print("divisor product: {}, item before: {}, item after: {}".format(monkey_v['divisor_product'],before,item))
+
+                if item_worry_level % monkey_v['test_divisor'] == 0:
+                    monkey_dict[monkey_v['if_true_target']]['inventory'].append(item_worry_level)
+                else: 
+                    monkey_dict[monkey_v['if_false_target']]['inventory'].append(item_worry_level)
+                monkey_v['inspected_count'] += 1
+
     for mon_key, monkey_v in monkey_dict.items():
-        for item in monkey_v['inventory'][:]:
-            #print("Monkey {} is inspecting item {}".format(mon_key,item))
-            monkey_v['inventory'].remove(item)
-            # item = int(item)
-            
-            # First perform the operation
-            if monkey_v['operation'][1] == 'old':
-                second_arg = item
-            else:
-                second_arg = int(monkey_v['operation'][1])
-            match monkey_v['operation'][0]:
-                case '+':
-                    item = item + second_arg
-                case '*':
-                    item = item * second_arg
-                case '-':
-                    item = item - second_arg
-                case '/':
-                    item = item / second_arg
+        #print("Monkey {} has the following inventory: {}".format(mon_key, monkey_v['inventory']))
+        print("Monkey {} has inspected {} items".format(mon_key, monkey_v['inspected_count']))
 
-            # Divide by floor(3) due to boredom
-            item = item//3
-
-            if item % monkey_v['test_divisor'] == 0:
-                monkey_dict[monkey_v['if_true_target']]['inventory'].append(item)
-                print("Monkey {} throws {} to {}".format(mon_key,item,monkey_v['if_true_target']))
-            else: 
-                monkey_dict[monkey_v['if_false_target']]['inventory'].append(item)
-                print("Monkey {} throws {} to {}".format(mon_key,item,monkey_v['if_false_target']))
-            monkey_v['inspected_count'] += 1
-    round_count += 1
-
-for mon_key, monkey_v in monkey_dict.items():
-    print("Monkey {} has the following inventory: {}".format(mon_key, monkey_v['inventory']))
-    print("Monkey {} has inspected {} items".format(mon_key, monkey_v['inspected_count']))
-
-inspected_counts = [monkey_dict[i]['inspected_count'] for i in monkey_dict.keys()]
-inspected_counts.sort()
-monkey_business = inspected_counts[-2] * inspected_counts[-1]
-print("Therefore, the level of monkey business is {}! Unacceptable!".format(monkey_business))
+for pt, args in enumerate([[20, False],[10000,True]],start=1):
+    monkeyGame(*args)
+    inspected_counts = [monkey_dict[i]['inspected_count'] for i in monkey_dict.keys()]
+    inspected_counts.sort()                                                                        
+    monkey_business = inspected_counts[-2] * inspected_counts[-1]
+    print("Therefore, the level of monkey business in part {} is {}! Unacceptable!\n".format(pt, monkey_business))
